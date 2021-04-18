f13825c4319f0fc6f16e64233dae86702db9802c
Ihor Tsoi <ihor.tsoi@nure.ua>
2021-04-18 21:01:59 +0300

    Server and client were developed.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly int defaultPort = 7777;
+        private static readonly IPAddress defaultIpAddress = IPAddress.Parse("127.0.0.1");
+        private static readonly string defaultEndOfMessageSymbol = "\n\n";
+
+        static void Main()
         {
-            Console.WriteLine(":: client wordking ...");
+            // the task uses unicode characters
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var clientSocket = new Socket(
+                AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            clientSocket.Connect(defaultIpAddress, defaultPort);
+            Console.WriteLine($"Connected to server [{defaultIpAddress}:{defaultPort}]");
+
+            Console.WriteLine("Please type the words to be sent. Use the \" \" as a delimiter.");
+            var requestMessage = Console.ReadLine();
+            SendMessage(clientSocket, requestMessage);
+            Console.WriteLine("Message was sent: " + requestMessage);
+
+            var responseMessage = ReceiveMessage(clientSocket);
+            Console.WriteLine("Response received: " + responseMessage);
+
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+
+        private static void SendMessage(Socket clientSocket, string requestMessage)
+        {
+            var bytesToBeSent = Encoding.Unicode.GetBytes(requestMessage + defaultEndOfMessageSymbol);
+            clientSocket.Send(bytesToBeSent);
+        }
+
+        private static string ReceiveMessage(Socket handler)
+        {
+            var receiveBuffer = new byte[1024];
+            var receivedMessageParts = new StringBuilder();
+
+            while (true)
+            {
+                int bytesReceivedCount = handler.Receive(receiveBuffer);
+                string messagePart = Encoding.Unicode.GetString(
+                    receiveBuffer, 0, bytesReceivedCount);
+                receivedMessageParts.Append(messagePart);
+
+                if (messagePart.Contains(defaultEndOfMessageSymbol))
+                {
+                    break;
+                }
+            }
+
+            return receivedMessageParts.ToString()
+                .TrimEnd(defaultEndOfMessageSymbol.ToCharArray());
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Server
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly int defaultPort = 7777;
+        private static readonly IPAddress defaultIpAddress = IPAddress.Any;
+        private static readonly string defaultEndOfMessageSymbol = "\n\n";
+
+        static void Main()
         {
-            Console.WriteLine(":: server wordking ...");
+            // the task uses unicode characters
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var listeningSocket = new Socket(
+                AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            listeningSocket.Bind(new IPEndPoint(defaultIpAddress, defaultPort));
+            listeningSocket.Listen(backlog: 1);
+            Console.WriteLine($"Listening on any IP address, port: {defaultPort}");
+
+
+            var handler = listeningSocket.Accept();
+            Console.WriteLine("Connection was accepted.");
+
+            var requestMessage = ReceiveMessage(handler);
+            Console.WriteLine("Request message was received: " + requestMessage);
+
+            var wordsToBeSentBack = PerformTask(requestMessage);
+            var responseMessage = string.Join(", ", wordsToBeSentBack);
+
+            SendMessage(handler, responseMessage);
+            Console.WriteLine("Response message was send: " + responseMessage);
+
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+        }
+
+        private static void SendMessage(Socket handler, string message)
+        {
+            byte[] bytesToBeSend = Encoding.Unicode.GetBytes(message + defaultEndOfMessageSymbol);
+            handler.Send(bytesToBeSend);
+        }
+
+        private static IEnumerable<string> PerformTask(string message) =>
+            message.Split(' ').Where(word => word.ToLower().EndsWith("во"));
+
+        private static string ReceiveMessage(Socket handler)
+        {
+            var receiveBuffer = new byte[1024];
+            var receivedMessageParts = new StringBuilder();
+
+            while (true)
+            {
+                int bytesReceivedCount = handler.Receive(receiveBuffer);
+                string messagePart = Encoding.Unicode.GetString(
+                    receiveBuffer, 0, bytesReceivedCount);
+                receivedMessageParts.Append(messagePart);
+
+                if (messagePart.Contains(defaultEndOfMessageSymbol))
+                {
+                    break;
+                }
+            }
+
+            return receivedMessageParts.ToString()
+                .TrimEnd(defaultEndOfMessageSymbol.ToCharArray());
         }
     }
 }
